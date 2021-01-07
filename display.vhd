7771fa7422f48ec11b71b4031abda5347483ab69
Mehmet Atay <mehmetatay257@gmail.com>
2021-01-07 20:06:42 +0300

    counter created 0 than 9 for 4 digit.

--- display.vhd
+++ display.vhd
@@ -16,178 +16,199 @@ end display;
 
 architecture Behavioral of display is
 
-signal use_digit_1, use_digit_2, use_digit_3, use_digit_4 : integer := 1;
+    signal use_digit_1, use_digit_2, use_digit_3, use_digit_4 : integer := 0;
+    signal digit_value : integer := 4;
+    signal counter_2 : integer := 0;
 
 begin
 
+    use_digit_1 <= digit_value;
+    use_digit_2 <= digit_value;
+    use_digit_3 <= digit_value;
+    use_digit_4 <= digit_value;
 
-process (clk_refresh)
-variable digit : std_logic_vector(1 downto 0) := (others => '0') ;
-begin
-    if clk_refresh'event and clk_refresh = '1' then
-        case( digit ) is
-        
-            when "00" =>
-                case( use_digit_1 ) is
-                    
-                    when 0  =>
-                        anode   <=  "1110";
-                        cathode <=  "0000001";
-                    when  1 =>
-                        anode   <=  "1110";
-                        cathode <=  "1001111";
-                    when  2 =>
-                        anode   <=  "1110";
-                        cathode <=  "0010010";    
-                    when  3 =>
-                        anode   <=  "1110";
-                        cathode <=  "0000110";    
-                    when  4 =>
-                        anode   <=  "1110";
-                        cathode <=  "1001100";
-                    when  5 =>
-                        anode   <=  "1110";
-                        cathode <=  "0100100";
-                    when  6 =>
-                        anode   <=  "1110";
-                        cathode <=  "0100000";
-                    when  7 =>
-                        anode   <=  "1110";
-                        cathode <=  "0001111";
-                    when  8 =>
-                        anode   <=  "1110";
-                        cathode <=  "0000000";
-                    when  9 =>
-                        anode   <=  "1110";
-                        cathode <=  "0000100";
-                    
-                    when others =>
+    process (clk_refresh)
+    variable digit : std_logic_vector(1 downto 0) := (others => '0') ;
+    begin
+        if clk_refresh'event and clk_refresh = '1' then
 
-                end case;
+            case( digit ) is
             
-            when "01" =>
+                when "00" =>
+                    case( use_digit_1 ) is
+                        
+                        when 0  =>
+                            anode   <=  "1110";
+                            cathode <=  "1000000";
+                        when  1 =>
+                            anode   <=  "1110";
+                            cathode <=  "1111001";
+                        when  2 =>
+                            anode   <=  "1110";
+                            cathode <=  "0100100";    
+                        when  3 =>
+                            anode   <=  "1110";
+                            cathode <=  "0110000";    
+                        when  4 =>
+                            anode   <=  "1110";
+                            cathode <=  "0011000";
+                        when  5 =>
+                            anode   <=  "1110";
+                            cathode <=  "0010010";
+                        when  6 =>
+                            anode   <=  "1110";
+                            cathode <=  "0000010";
+                        when  7 =>
+                            anode   <=  "1110";
+                            cathode <=  "1111000";
+                        when  8 =>
+                            anode   <=  "1110";
+                            cathode <=  "0000000";
+                        when  9 =>
+                            anode   <=  "1110";
+                            cathode <=  "0010000";
+                        
+                        when others =>
 
-                case ( use_digit_2 ) is
+                    end case;
                 
-                    when 0  =>
-                        anode   <=  "1101";
-                        cathode <=  "0000001";
-                    when  1 =>
-                        anode   <=  "1101";
-                        cathode <=  "1001111";
-                    when  2 =>
-                        anode   <=  "1101";
-                        cathode <=  "0010010";    
-                    when  3 =>
-                        anode   <=  "1101";
-                        cathode <=  "0000110";    
-                    when  4 =>
-                        anode   <=  "1101";
-                        cathode <=  "1001100";
-                    when  5 =>
-                        anode   <=  "1101";
-                        cathode <=  "0100100";
-                    when  6 =>
-                        anode   <=  "1101";
-                        cathode <=  "0100000";
-                    when  7 =>
-                        anode   <=  "1101";
-                        cathode <=  "0001111";
-                    when  8 =>
-                        anode   <=  "1101";
-                        cathode <=  "0000000";
-                    when  9 =>
-                        anode   <=  "1101";
-                        cathode <=  "0000100";
+                when "01" =>
+
+                    case ( use_digit_2 ) is
                     
-                    when others =>
+                        when 0  =>
+                            anode   <=  "1101";
+                            cathode <=  "1000000";
+                        when  1 =>
+                            anode   <=  "1101";
+                            cathode <=  "1111001";
+                        when  2 =>
+                            anode   <=  "1101";
+                            cathode <=  "0100100";    
+                        when  3 =>
+                            anode   <=  "1101";
+                            cathode <=  "0110000";    
+                        when  4 =>
+                            anode   <=  "1101";
+                            cathode <=  "0011001";
+                        when  5 =>
+                            anode   <=  "1101";
+                            cathode <=  "0010010";
+                        when  6 =>
+                            anode   <=  "1101";
+                            cathode <=  "0000010";
+                        when  7 =>
+                            anode   <=  "1101";
+                            cathode <=  "1111000";
+                        when  8 =>
+                            anode   <=  "1101";
+                            cathode <=  "0000000";
+                        when  9 =>
+                            anode   <=  "1101";
+                            cathode <=  "0010000";
+                        
+                        when others =>
 
-                end case;
-            
-            when "10" =>
-
-                case ( use_digit_3 ) is
+                    end case;
                 
-                    when 0  =>
-                        anode   <=  "1011";
-                        cathode <=  "0000001";
-                    when  1 =>
-                        anode   <=  "1011";
-                        cathode <=  "1001111";
-                    when  2 =>
-                        anode   <=  "1011";
-                        cathode <=  "0010010";    
-                    when  3 =>
-                        anode   <=  "1011";
-                        cathode <=  "0000110";    
-                    when  4 =>
-                        anode   <=  "1011";
-                        cathode <=  "1001100";
-                    when  5 =>
-                        anode   <=  "1011";
-                        cathode <=  "0100100";
-                    when  6 =>
-                        anode   <=  "1011";
-                        cathode <=  "0100000";
-                    when  7 =>
-                        anode   <=  "1011";
-                        cathode <=  "0001111";
-                    when  8 =>
-                        anode   <=  "1011";
-                        cathode <=  "0000000";
-                    when  9 =>
-                        anode   <=  "1011";
-                        cathode <=  "0000100";
+                when "10" =>
+
+                    case ( use_digit_3 ) is
                     
-                    when others =>
+                        when 0  =>
+                            anode   <=  "1011";
+                            cathode <=  "1000000";
+                        when  1 =>
+                            anode   <=  "1011";
+                            cathode <=  "1111001";
+                        when  2 =>
+                            anode   <=  "1011";
+                            cathode <=  "0100100";    
+                        when  3 =>
+                            anode   <=  "1011";
+                            cathode <=  "0110000";    
+                        when  4 =>
+                            anode   <=  "1011";
+                            cathode <=  "0011001";
+                        when  5 =>
+                            anode   <=  "1011";
+                            cathode <=  "0010010";
+                        when  6 =>
+                            anode   <=  "1011";
+                            cathode <=  "0000010";
+                        when  7 =>
+                            anode   <=  "1011";
+                            cathode <=  "1111000";
+                        when  8 =>
+                            anode   <=  "1011";
+                            cathode <=  "0000000";
+                        when  9 =>
+                            anode   <=  "1011";
+                            cathode <=  "0010000";
+                        
+                        when others =>
 
-                end case;
+                    end case;
 
-            when "11" =>
-                case( use_digit_4 ) is
+                when "11" =>
+                    case( use_digit_4 ) is
 
-                    when 0  =>
-                        anode   <=  "0111";
-                        cathode <=  "0000001";
-                    when  1 =>
-                        anode   <=  "0111";
-                        cathode <=  "1001111";
-                    when  2 =>
-                        anode   <=  "0111";
-                        cathode <=  "0010010";    
-                    when  3 =>
-                        anode   <=  "0111";
-                        cathode <=  "0000110";    
-                    when  4 =>
-                        anode   <=  "0111";
-                        cathode <=  "1001100";
-                    when  5 =>
-                        anode   <=  "0111";
-                        cathode <=  "0100100";
-                    when  6 =>
-                        anode   <=  "0111";
-                        cathode <=  "0100000";
-                    when  7 =>
-                        anode   <=  "0111";
-                        cathode <=  "0001111";
-                    when  8 =>
-                        anode   <=  "0111";
-                        cathode <=  "0000000";
-                    when  9 =>
-                        anode   <=  "0111";
-                        cathode <=  "0000100";
-                    
-                    when others =>
+                        when 0  =>
+                            anode   <=  "0111";
+                            cathode <=  "1000000";
+                        when  1 =>
+                            anode   <=  "0111";
+                            cathode <=  "1111001";
+                        when  2 =>
+                            anode   <=  "0111";
+                            cathode <=  "0100100";    
+                        when  3 =>
+                            anode   <=  "0111";
+                            cathode <=  "0110000";    
+                        when  4 =>
+                            anode   <=  "0111";
+                            cathode <=  "0011001";
+                        when  5 =>
+                            anode   <=  "0111";
+                            cathode <=  "0010010";
+                        when  6 =>
+                            anode   <=  "0111";
+                            cathode <=  "0000010";
+                        when  7 =>
+                            anode   <=  "0111";
+                            cathode <=  "1111000";
+                        when  8 =>
+                            anode   <=  "0111";
+                            cathode <=  "0000000";
+                        when  9 =>
+                            anode   <=  "0111";
+                            cathode <=  "0010000";
+                        
+                        when others =>
 
-                end case ;
+                    end case ;
+                
+                when others =>
+                
+            end case;          
             
-            when others =>
-             
-        end case;          
-        
-        digit := digit + 1;
-   
-    end if;
-end process;
+            digit := digit + 1;
+                        
+        end if;
+    end process;
+    
+    COUNTER : process (clk_refresh)
+    begin
+        if clk_refresh'event and clk_refresh = '1' then
+            counter_2 <= counter_2 + 1;
+            if counter_2 > 100000000 then
+                digit_value <= digit_value + 1;
+                if digit_value = 9 then
+                    digit_value <= 0;
+                end if;
+                counter_2 <= 0;
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
